438a2e1464b89ac9c9770e687c6716ca2f00d351
Marek Lewiński <lewinski.marek@outlook.com>
2016-09-13 17:28:46 +0200

    Added Nocs.Networking module

---
+++ Nocs/Nocs.Networking/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nocs.Networking
+{
+    public class Class1
+    {
+    }
+}
