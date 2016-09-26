303dcf2fb701be68400e161ca3240f4a0506f2ba
Marek Lewiński <lewinski.marek@outlook.com>
2016-09-26 11:38:20 +0200

    Implemented Nocs.Networking.ICMP.Tracert.GetHop() and created unit test

---
+++ Nocs/Nocs.Networking.Tests/TracertTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nocs.Networking.ICMP;
+using Nocs.Networking.Model;
+
+namespace Nocs.Networking.Tests
+{
+    [TestClass]
+    public class TracertTest
+    {
+        [TestMethod]
+        public void IsHopCorrect()
+        {
+            Tracert tracert = new Tracert();
+            HostInformation host = tracert.GetHop(new HostInformation() {Address = IPAddress.Parse("127.0.0.1")}, 1);
+            Assert.AreEqual(host.Address, IPAddress.Parse("127.0.0.1"));
+
+        }
+    }
+}
---
+++ Nocs/Nocs.Networking/ICMP/Tracert.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nocs.Networking.Model;
+
+namespace Nocs.Networking.ICMP
+{
+    public class Tracert
+    {
+        List<HostInformation> _hops = new List<HostInformation>();
+        Nocs.Networking.ICMP.Ping _pinger = new Ping();
+
+        public List<HostInformation> Hops
+        {
+            get; set;
+        }
+
+        public HostInformation GetHop(HostInformation target, int n, int timeout=10000)
+        {
+            _pinger.Host = target;
+            _pinger.Send(n,timeout);
+            return new HostInformation() {Address = _pinger.Messages.ToList()[0].Address, Hostname = String.Empty};
+        }
+
+    }
+}
